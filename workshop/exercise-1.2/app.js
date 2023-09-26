958db6c24df743f91bd6c85165c7bec03c050a2e
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-09-26 17:21:45 -0400

    Exercise 1.2

--- workshop/exercise-1.2/app.js
+++ workshop/exercise-1.2/app.js
@@ -1,3 +1,15 @@
 // Exercise 1.2
 // ------------
+
+const clickElement = document.querySelector("body")
+
+setTimeout ( () => {
+    clickElement.addEventListener ("click", () => {
+        result.innerText = "You Lose!"
+    })
+    console.log('exercise 1.2');
+},1000)
+clickElement.addEventListener ("click", () => {
+    result.innerText = "You Win!"
+})
 console.log('exercise 1.2');
