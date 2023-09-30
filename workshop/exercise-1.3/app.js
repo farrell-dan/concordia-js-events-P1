98899d8c8eada192c25fcc01cb0aac5062cc14a6
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-09-30 10:37:00 -0400

    Exercise 1.3

--- workshop/exercise-1.3/app.js
+++ workshop/exercise-1.3/app.js
@@ -1,3 +1,29 @@
 // Exercise 1.3
 // ------------
+
+const clickElement = document.querySelector("body")
+const time = document.getElementById("time");
+
+const randomSeconds = Math.floor(Math.random()*5);
+const randomMiliseconds = randomSeconds * 1000;
+
+time.innerText = randomSeconds
+
+setTimeout ( () => {
+    clickElement.addEventListener("click", () =>{
+        result.innerText = "You Lose!"
+    })
+}, randomMiliseconds)
+ 
+
+clickElement.addEventListener("click", () => {
+    result.innerText = "You Win!"
+})
+
+
+
+console.log(time);
+console.log(randomSeconds);
+console.log(randomMiliseconds);
+
 console.log('exercise 1.3');
